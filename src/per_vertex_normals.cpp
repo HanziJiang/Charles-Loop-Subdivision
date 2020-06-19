749946ca65c1fc46384b917b39036444d5267153
Hanzi Jiang <hanzi.jiang@mail.utoronto.ca>
2020-06-19 17:58:25 -0400

    Fix bug

--- src/per_vertex_normals.cpp
+++ src/per_vertex_normals.cpp
@@ -11,16 +11,16 @@ void per_vertex_normals(
   const int V_rows = V.rows();
   const int F_rows = F.rows();
 
-  Eigen::RowVector3d n;
+  Eigen::RowVector3d normal;
   for (int i = 0; i < V_rows; i++) {
-    n = Eigen::RowVector3d(0, 0, 0);
+    normal = Eigen::RowVector3d(0, 0, 0);
     for (int j = 0; j < F_rows; j++) {
       // if the triangle at F.row(j) has V.row(i) as one of its vertices, add the area-weighted normal of the triangle to n
       if (F(j, 0) == i || F(j, 1) == i || F(j, 2) == i) {
-        n += triangle_area_normal(V.row(F(i, 0)), V.row(F(i, 1)), V.row(F(i, 2)));
+        normal += triangle_area_normal(V.row(F(j, 0)), V.row(F(j, 1)), V.row(F(j, 2)));
       }
     }
-    N.row(i) = n.normalized();
+    N.row(i) = normal.normalized();
   }
   ////////////////////////////////////////////////////////////////////////////
 }
