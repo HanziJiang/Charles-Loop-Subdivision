80356b4183394f8709f91835f320ec5d9d8ccf34
Hanzi Jiang <hanzi.jiang@mail.utoronto.ca>
2020-06-26 19:42:17 -0400

    Refactor code

--- src/catmull_clark.cpp
+++ src/catmull_clark.cpp
@@ -33,22 +33,21 @@ void catmull_clark(
   const int F_rows = F.rows();
 
   int SV_size = V_rows + F_rows;
-  int SF_size = F_rows * 4;
+  const int SF_size = F_rows * 4;
 
+  // For each face, add face point.
   Eigen::MatrixXd face_points = Eigen::MatrixXd::Zero(F_rows, 3);
-
   for (int i = 0; i < F_rows; i++) {
-    // For each face, add face point
     face_points.row(i) = (V.row(F(i, 0)) + V.row(F(i, 1)) + V.row(F(i, 2)) + V.row(F(i, 3))) / 4.0;
   }
 
-  std::unordered_map<int, std::vector<int>> new_edges;  // vertex index & indices of points sharing edge with vertex
-  std::unordered_map<std::pair<int, int>, std::vector<int>, hash_pair> edge_to_faces;
+  std::unordered_map<int, std::vector<int>> new_edges;  // The key is a vertex represented by its index in V. The value is a list of vertex represented by their indices in V. Each new edge is stored only once. 
+  std::unordered_map<int, std::vector<Eigen::RowVector3d>> edge_points;  // The key is a vertex represented by its index in V. The value is a list of edge points. edge_points[i].at(j) is the edge point between the vertex V.row(i) and the vertex V.row(new_edges[i].at(j)). Each edge point is stored only once.
+  std::unordered_map<std::pair<int, int>, std::vector<int>, hash_pair> edge_to_faces; // The key represents an edge. The value is a list of faces, represented by their indices in F. An edge may be stored more than once.
   int current_vertex_index, another_vertex_index;
-  // vertex index & edge points of the edges vertex-edges[vertex index]
-  std::unordered_map<int, std::vector<Eigen::RowVector3d>> edge_points;
   std::pair<int, int> pair, reversed_pair;
 
+  // Populate edge_to_faces.
   for (int i = 0; i < F_rows; i++) {
     for (int j = 0; j < 4; j++) {
       current_vertex_index = F(i, j);
@@ -56,7 +55,6 @@ void catmull_clark(
       pair = std::make_pair(current_vertex_index, another_vertex_index);
       reversed_pair = std::make_pair(another_vertex_index, current_vertex_index);
       
-      // if the edge has not been stored
       if (edge_to_faces.find(pair) == edge_to_faces.end() && edge_to_faces.find(reversed_pair) == edge_to_faces.end()) {
         std::vector<int> entry;
         edge_to_faces[pair] = entry;
@@ -71,6 +69,7 @@ void catmull_clark(
 
   int num_vertex;
   Eigen::RowVector3d edge_point_value(0, 0, 0);
+  // Populate new_edges and edge_points.
   for (int i = 0; i < F_rows; i++) {
     for (int j = 0; j < 4; j++) {
       current_vertex_index = F(i, j);
@@ -78,14 +77,15 @@ void catmull_clark(
       pair = std::make_pair(current_vertex_index, another_vertex_index);
       reversed_pair = std::make_pair(another_vertex_index, current_vertex_index);
     
-      // if key not exist
+      // If key not exist.
       if (new_edges.find(current_vertex_index) == new_edges.end()) {
         std::vector<int> entry;
         new_edges[current_vertex_index] = entry;
         std::vector<Eigen::RowVector3d> edge_point_entry;
         edge_points[current_vertex_index] = edge_point_entry;
       }
-      // if vertex_index-target_index_1 is not already stored
+
+      // If the edge between vertex_index and target_index_1 is not already stored.
       if (std::find(new_edges[current_vertex_index].begin(), new_edges[current_vertex_index].end(), another_vertex_index) == new_edges[current_vertex_index].end() && (new_edges.find(another_vertex_index) == new_edges.end() || std::find(new_edges[another_vertex_index].begin(), new_edges[another_vertex_index].end(), current_vertex_index) == new_edges[another_vertex_index].end())) {
         new_edges[current_vertex_index].push_back(another_vertex_index);
         edge_point_value.setZero();
@@ -109,6 +109,7 @@ void catmull_clark(
 
   SV = Eigen::MatrixXd::Zero(SV_size, 3);
   SF = Eigen::MatrixXi::Zero(SF_size, 4);
+
   int num_edges;
   std::vector<std::vector<int> > VF;
   std::vector<int> face_indces;
@@ -118,7 +119,7 @@ void catmull_clark(
   Eigen::RowVector3d barycenter(0, 0, 0);
   int num_points;
   for (int i = 0; i < V_rows; i++) {
-    // calculate f, the average of all face points of faces adjacent to vertex at V.row(i)
+    // Calculate f, the average of all face points of faces adjacent to vertex at V.row(i).
     face_indces = VF[i];
     f.setZero();
     for (const int face_index : face_indces) {
@@ -127,7 +128,7 @@ void catmull_clark(
     f /= face_indces.size();
     
     num_edges = 0;
-    // calculate r, the average of all edge points of edges touching vertex at V.row(i)
+    // Calculate r, the average of all edge points of edges touching vertex at V.row(i).
     r.setZero();
     for (auto entry : edge_to_faces ) {
       if (entry.first.first == i) {
@@ -144,25 +145,25 @@ void catmull_clark(
 
     SV.row(i) = barycenter;
   }
-
+  
+  // Put each face point into SV.
   for (int i = 0; i < F_rows; i++) {
     SV.row(V_rows + i) = face_points.row(i);
   }
 
-  int size;
-  std::unordered_map<std::pair<int, int>, int, hash_pair> edge_to_SV_row;
+  std::unordered_map<std::pair<int, int>, int, hash_pair> edge_to_SV_row;  // The key is an edge. The value is the row number of the edge point in SV.
   int index = V_rows + F_rows;
+  // Put each edge point into SV and opulate edge_to_SV_row.
   for (int i = 0; i < V_rows; i++) {
-    size = edge_points[i].size();
-    for (int j = 0; j < size; j++) {
+    for (int j = 0; j < edge_points[i].size(); j++) {
       SV.row(index) = edge_points[i].at(j);
       std::pair<int, int> pair = std::make_pair(i, new_edges[i].at(j));
       edge_to_SV_row[pair] = index;
-      std::cout << "(" << pair.first << "," << pair.second << ")" << index << "\n";
       index++;
     }
   }
-
+  
+  // Populate SF.
   int point_index_1, point_index_2, point_index_3, point_index_4, another_index_1, another_index_2; 
   std::pair<int, int> pair_1, pair_1_reversed, pair_2, pair_2_reversed;
   for (int i = 0; i < F_rows; i++) {
@@ -182,7 +183,7 @@ void catmull_clark(
       } else if (edge_to_SV_row.find(pair_1_reversed) != edge_to_SV_row.end()) {
         point_index_2 = edge_to_SV_row[pair_1_reversed];
       } else {
-        //something went wrong
+        std::cerr << "Edge not stored in map";
       }
 
       if (edge_to_SV_row.find(pair_2) != edge_to_SV_row.end()) {
@@ -190,7 +191,7 @@ void catmull_clark(
       } else if (edge_to_SV_row.find(pair_2_reversed) != edge_to_SV_row.end()) {
         point_index_4 = edge_to_SV_row[pair_2_reversed];
       } else {
-        //something went wrong
+        std::cerr << "Edge not stored in map";
       }
 
       SF.row(i * 4 + j) = Eigen::RowVector4i(point_index_1, point_index_2, point_index_3, point_index_4);
@@ -198,10 +199,5 @@ void catmull_clark(
   }
 
   catmull_clark(Eigen::MatrixXd(SV), Eigen::MatrixXi(SF), num_iters - 1, SV, SF);
-
-  std::cout << SV << "\n";
-  std::cout << SF << "\n";
-
-
   ////////////////////////////////////////////////////////////////////////////
 }
